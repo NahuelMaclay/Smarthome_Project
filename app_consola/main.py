3bd1b2ed2bbe5373cc87af841c235260b48c9fdb
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2025-05-31 21:23:31 -0300

    nuevas funcionalidades : login , register , administracion de roles

--- app_consola/main.py
+++ app_consola/main.py
@@ -8,12 +8,15 @@ from states import states
 def main():
     print(f"----BIENVENIDO A SMART HOME SOLUTIONS----{datetime.datetime.now().strftime("%H:%M")}")
     activar_automatizacion_luces_del_patio()
-    # print("1. Registrarse")
-    # print("2. Iniciar sesión")
-    # print("3. Registrar nuevo dispositivo")
-    # print("4. Listar dispositivos")
-    # print("5. Buscar dispositivo")
-    # print("6. ELiminar un dispositivo")
+    #  mostrar_opciones_principal_de_app()
+    # if(states['is_auth']==False):
+    #     print("1. Registrarse")
+    #     print("2. Iniciar sesión")
+    # else:
+    #     print("3. Registrar nuevo dispositivo")
+    #     print("4. Listar dispositivos")
+    #     print("5. Buscar dispositivo")
+    #     print("6. ELiminar un dispositivo")
     # print("0. Salir")
     while True:
         mostrar_opciones_principal_de_app()
